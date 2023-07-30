2a13b653d61ef7a4d879fdb1f45f1fe19f4bfc95
raulett-homePC\raulett <raulett@gmail.com>
2023-07-31 00:59:33 +0800

    Initial commit

---
+++ functions.h
@@ -0,0 +1,21 @@
+//
+// Created by raulett on 31.07.2023.
+//
+
+#ifndef HW_COURCE_FIGURE_AREA_FUNCTIONS_H
+#define HW_COURCE_FIGURE_AREA_FUNCTIONS_H
+
+#include <math.h>
+
+double f1(double x){
+    return 0.6 * x + 3;
+}
+
+double f2(double x){
+    return pow((x - 2), 3) - 1;
+}
+
+double f3(double x){
+    return 3/x;
+}
+#endif //HW_COURCE_FIGURE_AREA_FUNCTIONS_H
